0c91724629d00908d18cc160bf035a04c6c541f1
konstantin <kosta123139@gmail.com>
2024-03-31 15:48:52 +0300

    hotfix

--- str_func.py
+++ str_func.py
@@ -7,3 +7,8 @@ def title_text(value):
     """docstring"""
     return value.title()
 
+
+print('внесены изменения')
+
+print(upper_text('это тестовый текст проверка'))
+print(title_text('это второй текст проверка'))
